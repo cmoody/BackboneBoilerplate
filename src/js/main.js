6c91a2ff8d5933878f132ed33bebe80f45dd4c8d
Chase Moody <chase.moody@mutualmobile.com>
2014-05-12 19:29:44 -0500

    Revert "integrate hammerjs"
    This reverts commit 2159fa6e59b5d2744fc4bd980de6a9f8daf63789.
    
    Conflicts:
    	src/js/main.js
    	todo.txt

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,13 @@ require.config({
 	 	'jquery': 'vendor/jquery/dist/jquery.min',
 	    'backbone': 'vendor/backbone/backbone',
 	    'underscore': 'vendor/underscore/underscore',
+<<<<<<< HEAD
 	    'hammerjs': 'vendor/jquery-hammerjs/jquery.hammer-full.min',
 	    'velocity': 'vendor/velocity/jquery.velocity.min',
+=======
+	    'hammerjs': 'vendor/hammerjs/hammer.min',
+	    'hammer-jquery': 'vendor/jquery-hammerjs/jquery.hammer.min',
+>>>>>>> parent of 2159fa6... integrate hammerjs
         'text' : 'vendor/requirejs-text/text',
 	    'app': 'app',
         'libs': 'libs'
@@ -15,9 +20,13 @@ require.config({
 });
 
 require([
+<<<<<<< HEAD
 	'app/router',
 	'velocity',
 	'libs/delegateEvents'
+=======
+	'app/router'
+>>>>>>> parent of 2159fa6... integrate hammerjs
 ], function(Router) {
 
 	var appRouter = new Router();
